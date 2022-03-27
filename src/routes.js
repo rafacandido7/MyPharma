9f60e527e02bea1ddbce13631feabed1fdac0558
Rafael Cândido <rafacandido0708@gmail.com>
2022-03-27 19:47:34 -0300

    Alteração na rota (index).

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 const express = require ('express');
-const {create, index, details, forget, update} = require ('./controllers/produtos.controller.js');
+const {index, create, details, forget, update} = require ('./controllers/produtos.controller.js');
 const routes = express.Router();
 
+
+//Para listar todos os produtos.
 routes.get('/', index);
 
 
-//Rotas de Produtos
+//Rotas dos Produtos.
 routes.post('/create_product', create);
 routes.get('/product_details/:_id', details);
 routes.delete('/delete_product/:_id', forget);
